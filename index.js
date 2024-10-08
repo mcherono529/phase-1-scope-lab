272658c57833931d7eaee29873977eea3a97b5c0
mercy <mcherono529@gmail.com>
2024-10-08 09:45:48 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+}
